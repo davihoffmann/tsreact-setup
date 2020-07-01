3f2fbec90a01febba08bb3a75cbd87409d3e74c6
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-07-01 10:14:49 -0300

    criado stato para o breadcrumb

--- src/main/components/MainLayout/Header/Breadcrumb/index.tsx
+++ src/main/components/MainLayout/Header/Breadcrumb/index.tsx
@@ -1,16 +1,11 @@
-import React, { ReactElement } from 'react';
-import { Link } from 'react-router-dom';
-import { Breadcrumb as AntdBreadcrumb } from 'antd';
+import { connect } from 'react-redux';
+import { AppState } from '../../../../app/store/types';
+import { LayoutState } from '../../../LayoutConfigurer/store/types';
+import { getBreadcrumb } from '../../../LayoutConfigurer/store';
+import Breadcrumb from './Breadcrumb';
 
-import { home } from '../../../../modules/Main/routes';
+const mapStateToProps = (state: AppState): Pick<LayoutState, 'breadcrumb'> => ({
+    breadcrumb: getBreadcrumb(state),
+});
 
-export default function Breadcrumb(): ReactElement {
-    return (
-        <AntdBreadcrumb>
-            <AntdBreadcrumb.Item>
-                <Link to={home.path}>Home</Link>
-            </AntdBreadcrumb.Item>
-            <AntdBreadcrumb.Item>Clientes</AntdBreadcrumb.Item>
-        </AntdBreadcrumb>
-    );
-}
+export default connect(mapStateToProps)(Breadcrumb);
